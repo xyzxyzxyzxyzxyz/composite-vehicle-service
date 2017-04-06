fa74a33fb29e4e69d8f0512e04dad7cbe4f7150f
XYZ <idm.caz@mailinator.com>
2017-04-06 16:48:07 +0200

    New test: CompositeController#constructor() Implementation

--- src/main/java/com/tdd/katas/microservices/composite/controller/CompositeController.java
+++ src/main/java/com/tdd/katas/microservices/composite/controller/CompositeController.java
@@ -1,4 +1,10 @@
 package com.tdd.katas.microservices.composite.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CompositeController {
+
+
+
 }
--- src/test/java/com/tdd/katas/microservices/composite/controller/CompositeControllerTest.java
+++ src/test/java/com/tdd/katas/microservices/composite/controller/CompositeControllerTest.java
@@ -3,11 +3,13 @@ package com.tdd.katas.microservices.composite.controller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
+@WebMvcTest(controllers = CompositeController.class)
 public class CompositeControllerTest {
 
     @Autowired
