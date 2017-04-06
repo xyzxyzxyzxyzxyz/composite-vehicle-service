76ca706940f9b715aca60880304648426531b9df
XYZ <idm.caz@mailinator.com>
2017-04-06 16:11:54 +0200

    Initial commit

---
+++ src/main/java/com/tdd/katas/microservices/CompositeVehicleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tdd.katas.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeVehicleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeVehicleServiceApplication.class, args);
+	}
+}
