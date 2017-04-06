043d685fef3a1d7bac7af05f745c954e975c91a7
XYZ <idm.caz@mailinator.com>
2017-04-06 19:34:44 +0200

    New test: CompositeController - Should return 404 NOT FOUND when asked for a non existent VIN. Implementation.

--- src/main/java/com/tdd/katas/microservices/composite/controller/CompositeController.java
+++ src/main/java/com/tdd/katas/microservices/composite/controller/CompositeController.java
@@ -3,6 +3,8 @@ package com.tdd.katas.microservices.composite.controller;
 import com.tdd.katas.microservices.composite.model.VehicleData;
 import com.tdd.katas.microservices.composite.service.CompositeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,8 +15,14 @@ public class CompositeController {
     private CompositeService compositeService;
 
     @GetMapping("/{vin}")
-    public VehicleData getVehicleData(@PathVariable String vin){
-        return compositeService.getVehicleData(vin);
+    public ResponseEntity<VehicleData> getVehicleData(@PathVariable String vin){
+        VehicleData vehicleData = compositeService.getVehicleData(vin);
+
+        if (vehicleData == null) {
+            return new ResponseEntity<VehicleData>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<VehicleData>(vehicleData, HttpStatus.OK);
     }
 
 }
