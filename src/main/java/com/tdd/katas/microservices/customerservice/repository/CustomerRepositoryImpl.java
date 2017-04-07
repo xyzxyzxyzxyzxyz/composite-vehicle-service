b46b0f435ba08fbf8f03d0717bbf8b0a96b97ede
XYZ <idm.caz@mailinator.com>
2017-04-07 16:18:34 +0200

    CustomerRepositoryImpl  New test: The repository returns valid output for valid input  Implementation

--- src/main/java/com/tdd/katas/microservices/customerservice/repository/CustomerRepositoryImpl.java
+++ src/main/java/com/tdd/katas/microservices/customerservice/repository/CustomerRepositoryImpl.java
@@ -3,13 +3,21 @@ package com.tdd.katas.microservices.customerservice.repository;
 import com.tdd.katas.microservices.vehicleservice.model.CustomerData;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+    private Map<String,CustomerData> map = new HashMap<>();
+
     public void store(String customer_id, CustomerData expectedCustomerData) {
+        map.put(customer_id, expectedCustomerData);
     }
 
     @Override
     public CustomerData getCustomerData(String customer_id) {
-        return null;
+        return map.get(customer_id);
     }
+
 }
