ee12d30d42747c63a8adef1d9c8c7d7e09bced41
XYZ <idm.caz@mailinator.com>
2017-04-07 18:22:55 +0200

    PartServiceImpl Tests & implementation.

---
+++ src/main/java/com/tdd/katas/microservices/partservice/service/PartServiceImpl.java
@@ -0,0 +1,21 @@
+package com.tdd.katas.microservices.partservice.service;
+
+import com.tdd.katas.microservices.partservice.repository.PartRepository;
+import com.tdd.katas.microservices.vehicleservice.model.PartData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PartServiceImpl implements PartService {
+
+    @Autowired
+    private PartRepository partRepository;
+
+    @Override
+    public List<PartData> getPartDataList(String vin) {
+        return partRepository.getPartDataList(vin);
+    }
+
+}
