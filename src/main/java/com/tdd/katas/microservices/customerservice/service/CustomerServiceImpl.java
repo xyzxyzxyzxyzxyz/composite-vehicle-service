d647a1c6f8aee356dcf079c1e93f0964353402b7
XYZ <idm.caz@mailinator.com>
2017-04-07 15:56:17 +0200

    CustomerServiceImpl  New test: The service delegates the calls to the repository  Implementation

--- src/main/java/com/tdd/katas/microservices/customerservice/service/CustomerServiceImpl.java
+++ src/main/java/com/tdd/katas/microservices/customerservice/service/CustomerServiceImpl.java
@@ -1,12 +1,18 @@
 package com.tdd.katas.microservices.customerservice.service;
 
+import com.tdd.katas.microservices.customerservice.repository.CustomerRepository;
 import com.tdd.katas.microservices.vehicleservice.model.CustomerData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @Override
     public CustomerData getCustomerData(String customer_id) {
-        return null;
+        return customerRepository.getCustomerData(customer_id);
     }
 }
