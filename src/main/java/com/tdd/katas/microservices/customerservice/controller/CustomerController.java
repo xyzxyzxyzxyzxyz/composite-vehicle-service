ad2516c42c77aa5e35a5d999f4066dd5939c33f4
XYZ <idm.caz@mailinator.com>
2017-04-07 15:33:02 +0200

    CustomerController New test: It returns valida data for a valid input Implementation.

--- src/main/java/com/tdd/katas/microservices/customerservice/controller/CustomerController.java
+++ src/main/java/com/tdd/katas/microservices/customerservice/controller/CustomerController.java
@@ -1,6 +1,11 @@
 package com.tdd.katas.microservices.customerservice.controller;
 
+import com.tdd.katas.microservices.customerservice.service.CustomerService;
 import com.tdd.katas.microservices.vehicleservice.model.CustomerData;
+import com.tdd.katas.microservices.vehicleservice.model.VehicleData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customers")
 public class CustomerController {
 
+    @Autowired
+    private CustomerService customerService;
+
     @GetMapping("/{customerId}")
-    public CustomerData getCustomerData(@PathVariable String customerId) {
-        return null;
+    public ResponseEntity<CustomerData> getCustomerData(@PathVariable String customerId) {
+        CustomerData customerData;
+
+        try {
+            customerData = customerService.getCustomerData(customerId);
+        } catch(Throwable error) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (customerData == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(customerData, HttpStatus.OK);
+
     }
 
 }
