c023839b1a4688256c5df97fb31f2b00ba5de8bb
XYZ <idm.caz@mailinator.com>
2017-04-07 10:35:22 +0200

    VehicleServiceImpl New test: The service should delegate the calls to the repository. Implementation.

--- src/main/java/com/tdd/katas/microservices/vehicleservice/service/VehicleServiceImpl.java
+++ src/main/java/com/tdd/katas/microservices/vehicleservice/service/VehicleServiceImpl.java
@@ -1,12 +1,19 @@
 package com.tdd.katas.microservices.vehicleservice.service;
 
 import com.tdd.katas.microservices.vehicleservice.model.VehicleData;
+import com.tdd.katas.microservices.vehicleservice.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VehicleServiceImpl implements VehicleService {
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
 
     @Override
     public VehicleData getVehicleData(String vin) {
-        return null;
+        return vehicleRepository.getVehicleData(vin);
     }
 
 }
