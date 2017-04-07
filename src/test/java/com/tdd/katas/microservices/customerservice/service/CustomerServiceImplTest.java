ad4fcc9c3498d54d1ea88ff269fdf8a6713eb265
XYZ <idm.caz@mailinator.com>
2017-04-07 16:01:45 +0200

    CustomerServiceImpl  New test: The service propagates the errors from the repository  Test & Implementation

--- src/test/java/com/tdd/katas/microservices/customerservice/service/CustomerServiceImplTest.java
+++ src/test/java/com/tdd/katas/microservices/customerservice/service/CustomerServiceImplTest.java
@@ -16,7 +16,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
@@ -42,6 +44,24 @@ public class CustomerServiceImplTest {
         verify(customerRepository).getCustomerData(CUSTOMER_ID);
 
         assertEquals("The service should return the CustomerData as provided by the repository", expectedCustomerData, actualCustomerData);
+    }
+
+    @Test
+    public void The_service_propagates_the_errors_from_the_repository() {
+
+        given(customerRepository.getCustomerData(any())).willThrow(new IllegalStateException("database not ready"));
+
+        try {
+            CustomerData actualCustomerData = customerRepository.getCustomerData(any());
+            fail("Should have thrown an exception");
+        } catch (IllegalStateException e) {
+            // The error has been propagated by the service
+        }
+
+        // The service must delegate the call to the repository with the same input
+        verify(customerRepository).getCustomerData(any());
+
+
     }
 
 }
