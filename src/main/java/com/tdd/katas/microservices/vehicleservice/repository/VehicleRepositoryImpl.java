b22043078ca40531fa121b2796a0a8a8d27c35a6
XYZ <idm.caz@mailinator.com>
2017-04-07 11:57:07 +0200

    VehicleServiceImpl new test: The repository returns valid output for valid input Implementation

--- src/main/java/com/tdd/katas/microservices/vehicleservice/repository/VehicleRepositoryImpl.java
+++ src/main/java/com/tdd/katas/microservices/vehicleservice/repository/VehicleRepositoryImpl.java
@@ -3,14 +3,20 @@ package com.tdd.katas.microservices.vehicleservice.repository;
 import com.tdd.katas.microservices.vehicleservice.model.VehicleData;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class VehicleRepositoryImpl implements VehicleRepository{
-    void store(String vin, VehicleData expectedVehicleData) {
 
+    private Map<String, VehicleData> map = new HashMap<>();
+
+    void store(String vin, VehicleData vehicleData) {
+        map.put(vin, vehicleData);
     }
 
     @Override
     public VehicleData getVehicleData(String vin) {
-        return null;
+        return map.get(vin);
     }
 }
