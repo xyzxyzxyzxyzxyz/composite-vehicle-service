0240a757d111d3219ee8e4dd6c465a8dceeeb8cc
XYZ <idm.caz@mailinator.com>
2017-04-07 15:36:29 +0200

    CustomerController New test: It returns valida data for a valid input Implementation.

--- src/main/java/com/tdd/katas/microservices/customerservice/controller/CustomerController.java
+++ src/main/java/com/tdd/katas/microservices/customerservice/controller/CustomerController.java
@@ -2,7 +2,6 @@ package com.tdd.katas.microservices.customerservice.controller;
 
 import com.tdd.katas.microservices.customerservice.service.CustomerService;
 import com.tdd.katas.microservices.vehicleservice.model.CustomerData;
-import com.tdd.katas.microservices.vehicleservice.model.VehicleData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
