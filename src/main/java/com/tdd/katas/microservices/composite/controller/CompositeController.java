d8466470f4823b159b98e85d36b9ff2bc77eb00c
XYZ <idm.caz@mailinator.com>
2017-04-07 09:34:28 +0200

    New test: CompositeController - Should return 500 INTERNAL SERVER ERROR when the service throws an error. Implementation

--- src/main/java/com/tdd/katas/microservices/composite/controller/CompositeController.java
+++ src/main/java/com/tdd/katas/microservices/composite/controller/CompositeController.java
@@ -16,7 +16,13 @@ public class CompositeController {
 
     @GetMapping("/{vin}")
     public ResponseEntity<VehicleData> getVehicleData(@PathVariable String vin){
-        VehicleData vehicleData = compositeService.getVehicleData(vin);
+        VehicleData vehicleData;
+
+        try {
+            vehicleData = compositeService.getVehicleData(vin);
+        } catch(Throwable error) {
+            return new ResponseEntity<VehicleData>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
         if (vehicleData == null) {
             return new ResponseEntity<VehicleData>(HttpStatus.NOT_FOUND);
