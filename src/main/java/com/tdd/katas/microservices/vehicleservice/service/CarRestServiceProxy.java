78baf62130e78e79e4b9398bf968b2fc02dcffdc
XYZ <idm.caz@mailinator.com>
2017-04-08 20:30:02 +0200

    CarRestServiceProxyTest new test: It_returns_null_if_the_vin_does_not_exist Implementation

--- src/main/java/com/tdd/katas/microservices/vehicleservice/service/CarRestServiceProxy.java
+++ src/main/java/com/tdd/katas/microservices/vehicleservice/service/CarRestServiceProxy.java
@@ -1,8 +1,10 @@
 package com.tdd.katas.microservices.vehicleservice.service;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -19,7 +21,17 @@ public class CarRestServiceProxy {
     }
 
     public Map<String, Object> getCarData(String vinCode) {
-        ResponseEntity<HashMap> responseEntity = restTemplate.getForEntity(URL + "/" + vinCode, HashMap.class);
-        return responseEntity.getBody();
+        try {
+            ResponseEntity<HashMap> responseEntity = restTemplate.getForEntity(URL + "/" + vinCode, HashMap.class);
+            return responseEntity.getBody();
+        }
+        catch(HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                return null;
+            }
+            else {
+                throw e;
+            }
+        }
     }
 }
