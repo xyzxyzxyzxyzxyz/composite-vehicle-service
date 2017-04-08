83b0818438ab27626c890423206d7af075a1b62e
XYZ <idm.caz@mailinator.com>
2017-04-08 18:51:32 +0200

    CustomerRestServiceProxyTest new test: It_returns_valid_data_for_a_valid_input Implementation

--- src/main/java/com/tdd/katas/microservices/vehicleservice/service/CustomerRestServiceProxy.java
+++ src/main/java/com/tdd/katas/microservices/vehicleservice/service/CustomerRestServiceProxy.java
@@ -1,21 +1,26 @@
 package com.tdd.katas.microservices.vehicleservice.service;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 class CustomerRestServiceProxy {
 
     private final RestTemplate restTemplate;
+    private static String URL = "/customers";
 
     public CustomerRestServiceProxy(RestTemplateBuilder restTemplateBuilder) {
         restTemplate = restTemplateBuilder.build();
     }
 
     public Map<String,Object> getCustomerData(String customerId) {
-        return  null;
+        ResponseEntity<HashMap> responseEntity = restTemplate.getForEntity(URL + "/" + customerId, HashMap.class);
+        return responseEntity.getBody();
     }
+
 }
