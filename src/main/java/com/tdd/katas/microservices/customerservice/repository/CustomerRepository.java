f75482e1973769141d23b58e9ef08ffa9d2ba84d
XYZ <idm.caz@mailinator.com>
2017-04-09 15:49:42 +0200

    Application refactoring. Moved the code about the customer-microservice to its own application

--- src/main/java/com/tdd/katas/microservices/customerservice/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tdd.katas.microservices.customerservice.repository;
-
-
-import com.tdd.katas.microservices.customerservice.model.CustomerData;
-
-public interface CustomerRepository {
-    CustomerData getCustomerData(String customer_id);
-}
