aa6c7b1b5a9660840e3a36425f1407bc8383fc08
XYZ <idm.caz@mailinator.com>
2017-04-09 16:14:59 +0200

    Application refactoring. Moved the code about the car-microservice to its own application

--- src/main/java/com/tdd/katas/microservices/carservice/repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tdd.katas.microservices.carservice.repository;
-
-
-import com.tdd.katas.microservices.carservice.model.CarData;
-
-public interface CarRepository {
-    CarData getCarData(String vin);
-}
--- src/main/java/com/tdd/katas/microservices/carservice/service/CarService.java
+++
@@ -1,7 +0,0 @@
-package com.tdd.katas.microservices.carservice.service;
-
-import com.tdd.katas.microservices.carservice.model.CarData;
-
-public interface CarService {
-    CarData getCarData(String vin);
-}
